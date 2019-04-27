aa8db44fe87ff736011e63c02414392a918819d6
alexandreReys <anr.alexandre@gmail.com>
2019-04-27 17:44:56 -0300

    alterado de https para http

--- backend/dist/express.js
+++ backend/dist/express.js
@@ -3,11 +3,10 @@ exports.__esModule = true;
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
-var fs = require("fs");
-var https = require("https");
+var http = require("http");
+//import * as https from 'https';
 var normalizePort = require("normalize-port");
 var auth_1 = require("./auth");
-var authz_1 = require("./authz");
 var routesContatos = require('./routes-contatos');
 var server = express();
 server.use(bodyParser.json());
@@ -25,14 +24,14 @@ server.get('/', function (req, res, next) {
     ;
 });
 server.post('/login', auth_1.handleAuthentication);
-server.use('/contatos', authz_1.handleAuthorization);
+// server.use('/contatos', handleAuthorization);
 server.use('/contatos', routesContatos);
-var options = {
-    cert: fs.readFileSync('./backend/keys/cert.pem'),
-    key: fs.readFileSync('./backend/keys/key.pem')
-};
+//const options = {
+//   cert: fs.readFileSync('./backend/keys/cert.pem'),
+//   key: fs.readFileSync('./backend/keys/key.pem')
+// };
 var port = normalizePort(process.env.atendWeb_port || '3000'); // Variavel Ambiental
 server.set('port', port);
-https.createServer(options, server).listen(port, function () {
+http.createServer(server).listen(port, function () {
     console.log("Server is running on port " + port);
 });
--- backend/express.ts
+++ backend/express.ts
@@ -2,7 +2,8 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as fs from 'fs';
-import * as https from 'https';
+import * as http from 'http';
+//import * as https from 'https';
 import * as normalizePort from 'normalize-port';
 
 import { handleAuthentication } from './auth';
@@ -26,16 +27,16 @@ server.get('/', (req, res, next) => {
 });
 
 server.post('/login', handleAuthentication);
-server.use('/contatos', handleAuthorization);
+// server.use('/contatos', handleAuthorization);
 server.use('/contatos', routesContatos);
 
-const options = {
-  cert: fs.readFileSync('./backend/keys/cert.pem'),
-  key: fs.readFileSync('./backend/keys/key.pem')
-};
+//const options = {
+//   cert: fs.readFileSync('./backend/keys/cert.pem'),
+//   key: fs.readFileSync('./backend/keys/key.pem')
+// };
 const port = normalizePort(process.env.atendWeb_port || '3000');  // Variavel Ambiental
 server.set('port', port);
 
-https.createServer(options, server).listen(port, () => {
+http.createServer(server).listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
