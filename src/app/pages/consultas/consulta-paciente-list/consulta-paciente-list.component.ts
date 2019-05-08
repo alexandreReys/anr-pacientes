50233b2c17a0e63d2beeae11a299db42c2b03395
alexandreReys <anr.alexandre@gmail.com>
2019-05-08 15:26:08 -0300

    add search bar

--- src/app/pages/consultas/consulta-paciente-list/consulta-paciente-list.component.ts
+++ src/app/pages/consultas/consulta-paciente-list/consulta-paciente-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { trigger, state, style, transition, animate} from "@angular/animations";
-
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { Observable, from } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/internal/operators';
 
 import { Contato } from '../../../models/contato.model';
 import { ContatoService } from 'src/app/services/contato.service';
@@ -20,18 +21,26 @@ import { ContatoService } from 'src/app/services/contato.service';
       state('visible', style({
         opacity: 1,
         'max-height': '70px',
-        'margin-top': '20px'
+        'margin-top': '1px'
       })),
       transition('* => *', animate('250ms 0s ease-in-out'))
     ])
   ]
 })
+
 export class ConsultaPacienteListComponent implements OnInit {
   
+  searchBarState = 'hidden';
+
+  searchForm: FormGroup;
+  searchControl: FormControl;
+
   contato: Contato;
   contatos: Contato[];
   
-  constructor(private contatoService: ContatoService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private contatoService: ContatoService) {}
 
   ngOnInit() {
     this.contato = new Contato();
@@ -39,6 +48,24 @@ export class ConsultaPacienteListComponent implements OnInit {
       .subscribe( contatos => { 
         this.contatos = contatos; 
       });
-  };
+      this.searchControl = this.formBuilder.control('');
+      this.searchForm = this.formBuilder.group({
+        searchControl: this.searchControl
+      });
+  
+      this.searchControl.valueChanges
+        .pipe( debounceTime( 500 ) )
+        .pipe( distinctUntilChanged() )
+        .pipe( switchMap( searchTerm => this.contatoService
+            .getContatos(searchTerm)
+            .pipe( catchError( function(error): Observable<any> { return from(['']) } ) )
+         ) )
+        .subscribe( contatos => this.contatos = contatos );
+    };
+  
+    toggleSearch() {
+      this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
+    }
+  
 
 }
